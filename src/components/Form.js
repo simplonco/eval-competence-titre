dda0f77bb140ea5743c682c30027d7422cba24b2
ShamShams <shamila.remtoula@hotmail.fr>
2017-11-16 18:28:03 +0100

    Edit List & Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,33 +1,6 @@
 import React, { Component } from 'react';
-import TextField from 'material-ui/TextField';
 
 class Form extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      species: 'Chat',
-      age: 'Adulte',
-      sex: 'Mâle',
-      affectionate: false,
-      quiet: false,
-      playful: false
-    };
-  }
-
-  handleChange = (event) => {
-    const name = event.target.name;
-    this.setState({ [name]: event.target.value })
-  }
-
-  toggle = (event) => {
-    const name = event.target.name;
-    this.setState({ [name]: !this.state.name })
-  }
-
-  // handleSubmit = (event) => {
-  //   event.preventDefault();
-  // }
-
   render() {
     return (
       <div className="Form">
@@ -43,14 +16,13 @@ class Form extends Component {
           <input type="text" name="entreprise"/><br/>
           <label>Email :</label>
           <input type="email" name="email"/><br/>
-          <label> Adress :</label>
+          <label> Adresse :</label>
           <input type="text" name="adresse"/><br/>
-          <button type="submit">Enregistrer</button>
+          <button type="submit">Ajouter</button>
         </form>
       </div>
     );
   }
-
 }
 
 export default Form;
